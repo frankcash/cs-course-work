58af077feb34043767f546541c75d190e7b33deb
frankcash <cashc@acm.org>
2016-08-30 21:43:25 -0400

    Adds info from lesson three

--- cop2220/lesson2/output.c
+++ cop2220/lesson2/output.c
@@ -7,6 +7,8 @@ int lessonOne(void){
     int numPeople; // creates a variable without initializing it
     int numDogs = 4 ; //  creates a variable and initializes it
     int foo, bar, fizz, buzz; // can define multiple vars at once
+
+    return 0;
 }
 
 /**
@@ -17,10 +19,22 @@ int lessonTwo(void){
   int bar = 4;
   foo = bar * 2;
   printf("foo is: %i\n", foo);
+
+  return 0;
+}
+
+/**
+* Identifiers
+*/
+int lessonThree(void){
+  printf("Identifiers cannot start with numbers\n");
+  printf("They also cannot contain spaces\n");
+
 }
 
 int main(void){
   lessonTwo();
+
   return 0;
 
 }
