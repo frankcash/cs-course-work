fd0889544b65f587620ee335887f5e76c6b47bc5
Frank Cash <cfcash@mtu.edu>
2016-09-05 17:07:39 -0400

    Adds functions to do area/ circumf/ diam

--- cop2220/homework/circle.c
+++ cop2220/homework/circle.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 
+float diameter(float r){
+
+  return (2 * r);
+}
+
+float circumference(float r){
+  float pi = 3.14159;
+  return (pi * 2 * r);
+}
+
+float area(float r){
+  float pi = 3.14159;
+
+  return (pi * r * r);
+}
+
 int main(void){
+
+  float radius;
+
+  printf("Please enter radius of the circle: \n");
+  scanf("%f", &radius);
+
+  printf("Radius is: %f", radius);
 
   return 0;
 }
