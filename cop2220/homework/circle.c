dbcd34c4507fb5ff99997cee84d6807a5540d4d6
frankcash <cashc@acm.org>
2016-09-06 16:43:01 -0400

    Finishes up homework 1

--- cop2220/homework/circle.c
+++ cop2220/homework/circle.c
@@ -1,29 +1,54 @@
+/**
+* Name: Charles Cash
+* Date: September 6, 2016
+* Class: COP 2220
+*/
+
 #include <stdio.h>
 
+/**
+* Calculates diameter
+*/
 float diameter(float r){
-
-  return (2 * r);
+  float res = (2 * r);
+  return res;
 }
 
+/**
+* Calculates circumference
+*/
 float circumference(float r){
   float pi = 3.14159;
-  return (pi * 2 * r);
+  float res = (pi * 2 * r);
+  return res;
 }
 
+/**
+* Calculates area
+*/
 float area(float r){
   float pi = 3.14159;
-
-  return (pi * r * r);
+  float res =(pi * r * r);
+  return res;
 }
 
+/**
+* Gets user input and then prints output
+*/
 int main(void){
 
-  float radius;
+  float radius, areaVal, circVal, diamVal;
 
   printf("Please enter radius of the circle: \n");
   scanf("%f", &radius);
 
-  printf("Radius is: %f", radius);
+  areaVal = area(radius);
+  circVal = circumference(radius);
+  diamVal = diameter(radius);
+
+  printf("Diameter is: %.2f\n", diamVal);
+  printf("Area is: %.2f\n", areaVal);
+  printf("Circumference is: %.2f\n", circVal);
 
   return 0;
 }
