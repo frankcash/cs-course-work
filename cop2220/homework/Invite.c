e4b00c3b06c4dbcc2f652eb0c3f955f3a68936fe
frankcash <cashc@acm.org>
2016-10-09 23:12:02 -0400

    Adds completed homework assignment

--- cop2220/homework/Invite.c
+++ cop2220/homework/Invite.c
@@ -1,18 +1,38 @@
 /**
 * Name: Charles Cash
-* Date: September 20, 2016
+* Date: October 09, 2016
 * Class: COP 2220
 * Assignment: Selections Quiz
+* Compiled with -lm flag in GCC
 */
 
 #include <stdio.h>
 #include <math.h>
 
+/**
+* Takes in the required parameters to calculate the total cost
+* Also prints final output
+*/
+int calcTotalPrice(int numPackageOne, int numPackageTwo, double pricePackageOne, double pricePackageTwo){
+
+  double totalPrice = 0.0;
+  totalPrice = (numPackageOne * pricePackageOne) + (numPackageTwo * pricePackageTwo);
+
+  printf("You should order %d small package(s).\n", numPackageOne);
+  printf("You should order %d large package(s).\n", numPackageTwo);
+  printf("Your cost for invitations will be $%.2lf.\n", totalPrice);
+
+  return 0;
+}
+
+/**
+* Main branch logic of program to calculate how to process pricing
+*/
 int main(void){
   int packageOne = 50;
   int packageTwo = 200;
-  int numPackageOne;
-  int numPackageTwo;
+  int numPackageOne = 0;
+  int numPackageTwo = 0;
 
   /** Vars from user input **/
   double pricePackageOne;
@@ -26,23 +46,43 @@ int main(void){
   printf("How many invitations are you sending out?\n");
   scanf("%d", &totalNumInvites);
 
-  printf("Vars are: %lf %lf %d\n", pricePackageOne, pricePackageTwo, totalNumInvites);
   double fourPricePackageOne = pricePackageOne * 4;
-  if(fabs(fourPricePackageOne - pricePackageTwo) < 0.01){
-    printf("diff is less than 0.001\n");
-    // configuration is indifferent
+  if(fabs(fourPricePackageOne - pricePackageTwo) < 60.0){
+    // go with a combination of both packages
+
+    numPackageTwo = totalNumInvites / packageTwo;
+    if(totalNumInvites % 200 != 0){
+      numPackageOne = ((totalNumInvites % 200) / packageOne);
+      if((totalNumInvites % 200) % 50 != 0){
+        numPackageOne += 1;
+      }
+    }
+
+    calcTotalPrice(numPackageOne, numPackageTwo, pricePackageOne, pricePackageTwo);
 
   }else if(fourPricePackageOne > pricePackageTwo ){
     // cheaper to go with a package two
 
+    numPackageTwo = totalNumInvites / packageTwo;
+    if(totalNumInvites % 200 != 0){
+        numPackageTwo += 1;
+    }
+
+    calcTotalPrice(0, numPackageTwo, pricePackageOne, pricePackageTwo);
+
   }else if(fourPricePackageOne < pricePackageTwo ){
     // cheaper to go with package ones
 
-  }else{
-    printf("%lf\n", (fabs(fourPricePackageOne - pricePackageTwo)));
+    numPackageOne = totalNumInvites / packageOne;
+    if(totalNumInvites % 50 != 0){
+      numPackageOne += 1;
+    }
+
+    calcTotalPrice(numPackageOne, 0, pricePackageOne, pricePackageTwo);
+
 
   }
 
-  // TODO: output
+
   return 0;
 }
