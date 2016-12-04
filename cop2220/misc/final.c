35517d27ce8902fc0b51a4394d5d7dc57b269f13
frankcash <cashc@acm.org>
2016-12-04 15:24:51 -0500

    Computations for my final exam

---
+++ cop2220/misc/final.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main(void){
+  // int integer1 = 2;
+  // int taxRate = 2;
+
+  // int count = 1;
+  // while(count <= 10){
+  //   printf("%i", count);
+  //   count = count + 1;
+  // }
+
+  // int i = 0;
+  // switch(i){
+  //   case(0):
+  //     printf("0\n");
+  //     break;
+  //   case(1):
+  //     printf("1\n");
+  //     break;
+  // }
+
+  int num1 = 10, num2 = 6, num3 = 4;
+  // if(num1 <= num2  || num3 >= 4){
+  //   printf("true\n");
+  // }
+
+  // if(num2 == num1 || num3 < num1 && num1 == 10){
+  //   printf("true\n");
+  // }else{
+  //   printf("false\n");
+  // }
+
+  // if(num1 <= num2 || num3 >= 4){
+  //   printf("true\n");
+  // }
+
+  if (num2 == num1 || num3 < num1 && num1 == 10){
+    printf("true\n");
+  }else{
+    printf("false\n");
+  }
+
+  // char foo[10];
+
+  // printf("%c", foo[4]);
+  return 0;
+}
